8c0d90a55c1efb1dbb886fd9dd27d9269b722ddf
Toady1104 <toady1104+github@hotmail.com>
2013-08-20 14:11:30 -0300

    "art." and "artigo" changes into O ARTIGO in one line of code

--- contentscript.js
+++ contentscript.js
@@ -12,8 +12,8 @@
 //if (regex.test(document.body.innerText))
 //{
 	//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("\\d\\d", "gi"), "XX"); //document.body.innerText
-	document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "art\\. \\d\\d\\d" , "gi"), "O ARTIGO"); //(("\\. ") || ("igo")) //("art\\.")|("artigo")
-	document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "artigo \\d\\d\\d" , "gi"), "O ARTIGO");
+	document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "(?:art\\.|artigo)" , "gi"), "O ARTIGO"); //(("\\. ") || ("igo")) //("art\\.")|("artigo")
+	//document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "artigo \\d\\d\\d" , "gi"), "O ARTIGO");
 	//document.body.innerHTML.replace(new RegExp("uno", "g"), "dos");
 	
 	// The regular expression produced a match, so notify the background page.
