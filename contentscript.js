62c94d2f2d78b341c9aee84813746da728767f72
Toady1104 <toady1104+github@hotmail.com>
2013-08-20 11:47:11 -0300

    Changes "the" to "XXX"

--- contentscript.js
+++ contentscript.js
@@ -3,12 +3,13 @@
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
-var regex = /Me/;
+
+var regex = /the/;
 
 // Test the text of the body element against our regular expression.
 if (regex.test(document.body.innerText))
 {
-	document.body.innerHTML = document.body.innerHTML.replace(new RegExp("me", "g"), "XX");
+	document.body.innerHTML = document.body.innerHTML.replace(new RegExp("the", "g"), "XXX"); //document.body.innerText
 	//document.body.innerHTML.replace(new RegExp("uno", "g"), "dos");
 	// The regular expression produced a match, so notify the background page.
 	chrome.extension.sendRequest({}, function(response) {});
