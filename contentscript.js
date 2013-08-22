eebb6dc080c58f5ef2669f1eeade347d2fb1050d
Toady1104 <toady1104+github@hotmail.com>
2013-08-22 13:01:11 -0300

    Added console debug

--- contentscript.js
+++ contentscript.js
@@ -4,21 +4,26 @@
  * LICENSE file.
  */
  
-//var regex = /art/;
+//DEBUG:
+//console.log ("TEST");
+ 
+var regex = /art/;
 //var regex2 = /artigo/;
 
 // art(?. )
 
-//if (regex.test(document.body.innerText))
-//{
+if (regex.test(document.body.innerText))
+{
 	//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("\\d\\d", "gi"), "XX"); //document.body.innerText
 	document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "(?:art\\.|artigo) " , "gi"), "O ARTIGO"); //(("\\. ") || ("igo")) //("art\\.")|("artigo")
 	//document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "artigo \\d\\d\\d" , "gi"), "O ARTIGO");
 	//document.body.innerHTML.replace(new RegExp("uno", "g"), "dos");
 	
+
+	
 	// The regular expression produced a match, so notify the background page.
 	chrome.extension.sendRequest({}, function(response) {});
-//} else
-//{
+} else
+{
 	// No match was found.
-//}
+}
