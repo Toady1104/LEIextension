d06623ae05b8664d85a5ca6ab902439156125eff
Toady1104 <toady1104+github@hotmail.com>
2013-08-22 14:16:32 -0300

    Matches art. or artigo n{1,4}

--- contentscript.js
+++ contentscript.js
@@ -3,8 +3,6 @@
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
 //import java.util.regex.Matcher;
 //import java.util.regex.Pattern;
 
@@ -12,41 +10,26 @@
 //console.log ("TEST");
  
 var regex = /art/;
-=======
  
 //var regex = /art/;
->>>>>>> parent of eebb6dc... Added console debug
-=======
  
 //var regex = /art/;
->>>>>>> parent of eebb6dc... Added console debug
 //var regex2 = /artigo/;
 
 // art(?. )
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 if (regex.test(document.body.innerText))
 {
-	document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "(?:art\\.|artigo) \\d{1,4}" , "gi"), "O ARTIGO"); //(("\\. ") || ("igo")) //("art\\.")|("artigo")
-	console.log ("line 20");
-=======
-=======
->>>>>>> parent of eebb6dc... Added console debug
 //if (regex.test(document.body.innerText))
 //{
 	//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("\\d\\d", "gi"), "XX"); //document.body.innerText
-	document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "(?:art\\.|artigo) " , "gi"), "O ARTIGO"); //(("\\. ") || ("igo")) //("art\\.")|("artigo")
+	document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "(?:art\\.|artigo) \\d{1,4}" , "gi"), "O ARTIGO"); //(("\\. ") || ("igo")) //("art\\.")|("artigo")
 	//document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "artigo \\d\\d\\d" , "gi"), "O ARTIGO");
 	//document.body.innerHTML.replace(new RegExp("uno", "g"), "dos");
-	
-<<<<<<< HEAD
->>>>>>> parent of eebb6dc... Added console debug
-=======
->>>>>>> parent of eebb6dc... Added console debug
 	// The regular expression produced a match, so notify the background page.
 	chrome.extension.sendRequest({}, function(response) {});
-//} else
-//{
+	console.log ("TEST");
+} else
+{
 	// No match was found.
-//}
+}
