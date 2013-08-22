053ff4d8d54b7dac74dd0e5a548dcf743e718bb4
Toady1104 <toady1104+github@hotmail.com>
2013-08-22 13:58:21 -0300

    Rolled back I guess

--- contentscript.js
+++ contentscript.js
@@ -3,12 +3,36 @@
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
+<<<<<<< HEAD
+<<<<<<< HEAD
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+
+//DEBUG:
+//console.log ("TEST");
+ 
+var regex = /art/;
+=======
  
 //var regex = /art/;
+>>>>>>> parent of eebb6dc... Added console debug
+=======
+ 
+//var regex = /art/;
+>>>>>>> parent of eebb6dc... Added console debug
 //var regex2 = /artigo/;
 
 // art(?. )
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+if (regex.test(document.body.innerText))
+{
+	document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "(?:art\\.|artigo) \\d{1,4}" , "gi"), "O ARTIGO"); //(("\\. ") || ("igo")) //("art\\.")|("artigo")
+	console.log ("line 20");
+=======
+=======
+>>>>>>> parent of eebb6dc... Added console debug
 //if (regex.test(document.body.innerText))
 //{
 	//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("\\d\\d", "gi"), "XX"); //document.body.innerText
@@ -16,6 +40,10 @@
 	//document.body.innerHTML = document.body.innerHTML.replace(new RegExp( "artigo \\d\\d\\d" , "gi"), "O ARTIGO");
 	//document.body.innerHTML.replace(new RegExp("uno", "g"), "dos");
 	
+<<<<<<< HEAD
+>>>>>>> parent of eebb6dc... Added console debug
+=======
+>>>>>>> parent of eebb6dc... Added console debug
 	// The regular expression produced a match, so notify the background page.
 	chrome.extension.sendRequest({}, function(response) {});
 //} else
